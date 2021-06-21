1fc9d513d3b8e6208e11fc542514cd61fbc64ee8
Felipe Catete <83040279+felipe-catete@users.noreply.github.com>
2021-06-21 09:39:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,24 +16,20 @@ async function currency() {
         return html += `<option value=${item}>${item}</option>`;
     })
 
+
+
     for(let i=0; i<select.length; i++) {
         select[i].innerHTML = html;
     }
-
-
 
     function convert (i, j) {
         input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value]
     }
 
     input[0].addEventListener("keyup", () => convert(1,0))
-
     input[1].addEventListener("keyup", () => convert(0,1))
-
     select[0].addEventListener("change", () => convert(1,0))
-
     select[1].addEventListener("change", () => convert(0,1))
-
-}
+} // The end of currency function is right here
 
 currency();
